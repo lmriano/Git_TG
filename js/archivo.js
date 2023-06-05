2b37c7d93fe8a345a28fbac58ef0906d2e3fa4e2
LauraRiano143 <lmriano41@ucatolica.edu.co>
2023-06-04 21:03:06 -0500

    adición de archivo pdf

--- js/archivo.js
+++ js/archivo.js
@@ -1,66 +1,60 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('.fecha');
-    const url = '../../php/archivo.php';
-    let data = []; // Variable para almacenar los datos
-  
-    // Obtener los datos del paciente actual al cargar la página
-    getData(url)
-      .then(resultData => {
-        data = resultData; // Almacenar los datos en la variable
-        displayData(data); // Mostrar los datos en la tabla
-      });
-  
-    const abrirBoton = document.getElementById('abrirBtn');
-    abrirBoton.addEventListener('click', function() {
-      const checkboxSeleccionado = document.querySelector('.table__body input[type="checkbox"]:checked');
-      if (checkboxSeleccionado) {
-        const pacienteSeleccionado = data.find(paciente => paciente.tiene_ECG === 'Sí');
-        if (pacienteSeleccionado) {
-          window.location.href = 'documento.html';
-        } else {
-          alert('No hay pacientes con ECG disponible');
-        }
-      } else {
-        alert('Debes seleccionar un paciente');
-      }
+  const url = '../../php/archivo.php';
+  let data = []; // Variable para almacenar los datos
+
+  // Obtener los datos del paciente actual al cargar la página
+  getData(url)
+    .then(resultData => {
+      data = resultData; // Almacenar los datos en la variable
+      displayData(data); // Mostrar los datos en la tabla
     });
-  });
-  
-  function getData(url) {
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Error en la solicitud: ' + response.status);
-        }
-        return response.json();
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-  
-  function displayData(data) {
-    const tableBody = document.querySelector('.table__body tbody');
-    tableBody.innerHTML = '';
-  
-    if (data.length === 0) {
-      console.log('No hay datos disponibles');
-      alert('No hay datos disponibles');
-      return;
+
+  const abrirBoton = document.getElementById('abrirBtn');
+  abrirBoton.addEventListener('click', function(event) {
+    const checkboxSeleccionado = document.querySelector('.table__body input[type="checkbox"]:checked');
+    if (checkboxSeleccionado) {
+      event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+      window.open('../../php/reportes/DescargarReporte_x_fecha_PDF.php', '_blank'); // Abrir enlace en una nueva pestaña
+    } else {
+      alert('Debes seleccionar un paciente');
     }
-  
-    data.forEach(paciente => {
-      if (paciente.tiene_ECG === 'Sí') {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-          <td><input type="checkbox"></td>
-          <td>${paciente.primer_nombre}</td>
-          <td>${paciente.primer_apellido}</td>
-          <td>${paciente.num_documento}</td>
-          <td>${paciente.fecha_consulta}</td>
-        `;
-        tableBody.appendChild(row);
+  });
+});
+
+function getData(url) {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error en la solicitud: ' + response.status);
       }
+      return response.json();
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
+}
+
+function displayData(data) {
+  const tableBody = document.querySelector('.table__body tbody');
+  tableBody.innerHTML = '';
+
+  if (data.length === 0) {
+    console.log('No hay datos disponibles');
+    alert('No hay datos disponibles');
+    return;
   }
-  
+
+  data.forEach(paciente => {
+    if (paciente.tiene_ECG === 'Sí') {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td><input type="checkbox"></td>
+        <td>${paciente.primer_nombre}</td>
+        <td>${paciente.primer_apellido}</td>
+        <td>${paciente.num_documento}</td>
+        <td>${paciente.fecha_consulta}</td>
+      `;
+      tableBody.appendChild(row);
+    }
+  });
+}
--- php/reportes/DescargarReporte_x_fecha_PDF.php
+++ php/reportes/DescargarReporte_x_fecha_PDF.php
@@ -1,41 +1,38 @@
 <?php
 require_once('tcpdf/tcpdf.php'); //Llamando a la Libreria TCPDF
-require_once('config.php'); //Llamando a la conexión para BD
-date_default_timezone_set('America/Bogota');
 
+date_default_timezone_set('America/Bogota');
 
 ob_end_clean(); //limpiar la memoria
 
-
 class MYPDF extends TCPDF{
       
-    	public function Header() {
-            $bMargin = $this->getBreakMargin();
-            $auto_page_break = $this->AutoPageBreak;
-            $this->SetAutoPageBreak(false, 0);
-            $img_file = dirname( __FILE__ ) .'../../media/logo-p.png';
-            $this->Image($img_file, 85, 8, 20, 25, '', '', '', false, 30, '', false, false, 0);
-            $this->SetAutoPageBreak($auto_page_break, $bMargin);
-            $this->setPageMark();
-	    }
+    public function Header() {
+        $bMargin = $this->getBreakMargin();
+        $auto_page_break = $this->AutoPageBreak;
+        $this->SetAutoPageBreak(false, 0);
+        $img_file = dirname(__FILE__) . '../../media/logo-p.png';
+        $this->Image($img_file, 85, 8, 20, 25, '', '', '', false, 30, '', false, false, 0);
+        $this->SetAutoPageBreak($auto_page_break, $bMargin);
+        $this->setPageMark();
+    }
 }
-
 
 //Iniciando un nuevo pdf
 $pdf = new MYPDF(PDF_PAGE_ORIENTATION, 'mm', 'Letter', true, 'UTF-8', false);
- 
+
 //Establecer margenes del PDF
 $pdf->SetMargins(20, 35, 25);
 $pdf->SetHeaderMargin(20);
 $pdf->setPrintFooter(false);
 $pdf->setPrintHeader(true); //Eliminar la linea superior del PDF por defecto
 $pdf->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM); //Activa o desactiva el modo de salto de página automático
- 
+
 //Informacion del PDF
 $pdf->SetCreator('SPORTHOLTER');
 $pdf->SetAuthor('SPORTHOLTER');
 $pdf->SetTitle('Informe de Pacientes');
- 
+
 /** Eje de Coordenadas
  *          Y
  *          -
@@ -52,74 +49,101 @@ $pdf->SetTitle('Informe de Pacientes');
 
 //Agregando la primera página
 $pdf->AddPage();
-$pdf->SetFont('helvetica','B',10); //Tipo de fuente y tamaño de letra
+$pdf->SetFont('helvetica', 'B', 10); //Tipo de fuente y tamaño de letra
 $pdf->SetXY(150, 20);
 $pdf->Write(0, 'Código: 0014ABC');
 $pdf->SetXY(150, 25);
-$pdf->Write(0, 'Fecha: '. date('d-m-Y'));
+$pdf->Write(0, 'Fecha: ' . date('d-m-Y'));
 $pdf->SetXY(150, 30);
-$pdf->Write(0, 'Hora: '. date('h:i A'));
+$pdf->Write(0, 'Hora: ' . date('h:i A'));
 $pdf->SetXY(150, 35);
 $pdf->Write(0, 'Ciudad: Bogota.D.C');
 
-$deporte ='Baloncesto';
-$pdf->SetFont('helvetica','B',10); //Tipo de fuente y tamaño de letra
+$deporte = 'Baloncesto';
+$pdf->SetFont('helvetica', 'B', 10); //Tipo de fuente y tamaño de letra
 $pdf->SetXY(15, 20); //Margen en X y en Y
-$pdf->SetTextColor(204,0,0);
+$pdf->SetTextColor(204, 0, 0);
 $pdf->Write(0, 'Especialista: Laura R');
 $pdf->SetTextColor(0, 0, 0); //Color Negrita
 $pdf->SetXY(15, 25);
 $pdf->Write(0, 'SPORTHOLTER');
 
-
-
 $pdf->Ln(35); //Salto de Linea
-$pdf->Cell(40,26,'',0,0,'C');
-/*$pdf->SetDrawColor(50, 0, 0, 0);
-$pdf->SetFillColor(100, 0, 0, 0); */
-$pdf->SetTextColor(34,68,136);
-//$pdf->SetTextColor(255,204,0); //Amarillo
-//$pdf->SetTextColor(34,68,136); //Azul
-//$pdf->SetTextColor(153,204,0); //Verde
-//$pdf->SetTextColor(204,0,0); //Marron
-//$pdf->SetTextColor(245,245,205); //Gris claro
-//$pdf->SetTextColor(100, 0, 0); //Color Carne
-$pdf->SetFont('helvetica','B', 15); 
-$pdf->Cell(100,6,'INFORME MEDICO-DEPORTIVO',0,0,'C');
-
+$pdf->Cell(40, 26, '', 0, 0, 'C');
+$pdf->SetTextColor(34, 68, 136);
+$pdf->SetFont('helvetica', 'B', 15);
+$pdf->Cell(100, 6, 'INFORME MEDICO-DEPORTIVO', 0, 0, 'C');
 
 $pdf->Ln(10); //Salto de Linea
-$pdf->SetTextColor(0, 0, 0); 
+$pdf->SetTextColor(0, 0, 0);
 
 //Almando la cabecera de la Tabla
-$pdf->SetFillColor(232,232,232);
-$pdf->SetFont('helvetica','B',12); //La B es para letras en Negritas
-$pdf->Cell(40,6,'Nombre',1,0,'C',1);
-$pdf->Cell(60,6,'Email',1,0,'C',1);
-$pdf->Cell(35,6,'Deporte',1,0,'C',1);
-$pdf->Cell(35,6,'Numero Documento',1,1,'C',1); 
+$pdf->SetFillColor(232, 232, 232);
+$pdf->SetFont('helvetica', 'B', 12); //La B es para letras en Negritas
+$pdf->Cell(40, 6, 'Nombre', 1, 0, 'C', 1);
+$pdf->Cell(60, 6, 'Email', 1, 0, 'C', 1);
+$pdf->Cell(35, 6, 'Deporte', 1, 0, 'C', 1);
+$pdf->Cell(35, 6, 'Numero Documento', 1, 1, 'C', 1); 
 /*El 1 despues de  Fecha Ingreso indica que hasta alli 
 llega la linea */
 
-$pdf->SetFont('helvetica','',10);
+$pdf->SetFont('helvetica', '', 10);
 
+require_once('../conexion.php');
 
-//SQL para consultas Deportistas
+$host = 'localhost';
+$port = '3306';
+$dbname = 'sportholter';
+$username = 'root';
+$password = '';
 
-$sqlPacientes = ('SELECT COUNT(*) AS count FROM usuarios WHERE num_documento = :documento AND id_tipo = 3');
-//$sqlTrabajadores = ("SELECT * FROM trabajadores");
-$query = mysqli_query($con, $sqlPacientes);
+try {
+    $conexion = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch(PDOException $error) {
+    echo "Error en la conexión: " . $error->getMessage();
+    die();
+}
 
-while ($dataRow = mysqli_fetch_array($query)) {
-        $pdf->Cell(40,6,($dataRow['primer_nombre']),1,0,'C');
-        $pdf->Cell(60,6,$dataRow['correo'],1,0,'C');
-        $pdf->Cell(35,6,($dataRow['ciudad_expedicion']),1,0,'C');
-        $pdf->Cell(35,6,($dataRow['num_documento']),1,1,'C');
+try {
+
+    $sqlPacientes = 'SELECT primer_nombre, correo, ciudad_expedicion, num_documento FROM usuarios WHERE num_documento = :documento AND id_tipo = 3';
+    $stmt = $conexion->prepare($sqlPacientes);
+    $stmt->bindParam(':documento', $documento);
+    $stmt->execute();
+    
+    while ($dataRow = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        if (isset($dataRow['primer_nombre'])) {
+            $pdf->Cell(40, 6, $dataRow['primer_nombre'], 1, 0, 'C');
+        } else {
+            $pdf->Cell(40, 6, '', 1, 0, 'C');
+        }
+    
+        if (isset($dataRow['correo'])) {
+            $pdf->Cell(60, 6, $dataRow['correo'], 1, 0, 'C');
+        } else {
+            $pdf->Cell(60, 6, '', 1, 0, 'C');
+        }
+    
+        if (isset($dataRow['ciudad_expedicion'])) {
+            $pdf->Cell(35, 6, $dataRow['ciudad_expedicion'], 1, 0, 'C');
+        } else {
+            $pdf->Cell(35, 6, '', 1, 0, 'C');
+        }
+    
+        if (isset($dataRow['num_documento'])) {
+            $pdf->Cell(35, 6, $dataRow['num_documento'], 1, 1, 'C');
+        } else {
+            $pdf->Cell(35, 6, '', 1, 1, 'C');
+        }
     }
+    
+} catch (PDOException $e) {
+    echo 'Error de conexión a la base de datos: ' . $e->getMessage();
+}
 
-
-//$pdf->AddPage(); //Agregar nueva Pagina
-
-$pdf->Output('Resumen_Pedido_'.date('d_m_y').'.pdf', 'I'); 
+$pdf->Output('Resumen_Pedido_' . date('d_m_y') . '.pdf', 'I');
 // Output funcion que recibe 2 parameros, el nombre del archivo, ver archivo o descargar,
 // La D es para Forzar una descarga
+?>
